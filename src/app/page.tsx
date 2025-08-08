4bccf1706e9e7e9d118e4de09e98f39654808de5
awazago <98982859+awazago@users.noreply.github.com>
2025-08-08 17:20:09 -0300

    Correcao pricinge page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function HomePage() {
         aspect_ratio: aspectRatio,
       };
 
-      const response = await fetch("http://127.0.0.1:8000/images/generate", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/images/generate`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
